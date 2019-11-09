33c918e4cb469fcbb0391577ce7871318375c5e4
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-09 12:58:44 +0000

    09_kadai

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+09の課題３
+Route::get('XXX', 'AaaController@bbb');
+*/
+
+
+/*
+09の課題４
+*/
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('news/create','Admin\NewsController@add');
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
+
+
+
+
+
