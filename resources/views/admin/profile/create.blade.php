0531257d82ae8fda094430d10ea4cce0c64d5e0c
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-10 04:31:08 +0000

    10_kadai

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>profile/create.blade.phpの画面</h1>
+    </body>
+</html>
