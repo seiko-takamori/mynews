c11c06b9c1d67dfe0a372652b330f2a09985585f
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-11 07:44:08 +0000

    11_kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>profile/create.blade.phpの画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+
+@section('title', 'MyNews')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div><!--col-md-8 mx-auto-->
+        </div><!--row-->
+    </div><!--container-->
+    
+@endsection
