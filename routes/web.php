4594e621b6a30035b5916956f3030af08117d4bf
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-14 09:54:56 +0000

    12_kadai

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,18 @@ Route::get('XXX', 'AaaController@bbb');
 */
 
 
-/*
-09の課題４
-*/
+
 Route::group(['prefix' => 'admin'],function(){
-    Route::get('news/create','Admin\NewsController@add');
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
 });
 
 
 
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
