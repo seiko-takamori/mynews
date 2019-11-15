d5976a27f472968e02e09837fe350f94b754c6ba
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-15 09:10:18 +0000

    13_kadai

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,6 +7,47 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My プロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">氏名(name)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">性別(gender)</label>
+                        <div class="col-md-10">
+                            <input type="radio" name="gender" value="{{ old('gender_m') }}">男
+                            <input type="radio" name="gender" value="{{ old('gender_f') }}">女
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2">趣味(hobby)</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('hobby') }}">
+                        </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄(introduction)</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="10">{{ old('introduction') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
+                
+                
             </div><!--col-md-8 mx-auto-->
         </div><!--row-->
     </div><!--container-->
--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-09の課題３
-Route::get('XXX', 'AaaController@bbb');
-*/
 
-
-
-Route::group(['prefix' => 'admin'],function(){
-    Route::get('profile/edit','Admin\ProfileController@edit')->middleware('auth');
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); 
+     Route::get('profile/create', 'Admin\ProfileController@add');
+     Route::post('profile/create', 'Admin\ProfileController@create'); 
+     Route::get('profile/edit', 'Admin\ProfileController@edit');
+     Route::post('profile/edit', 'Admin\ProfileController@updata'); 
 });
 
 
