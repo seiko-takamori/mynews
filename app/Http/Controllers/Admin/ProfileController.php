5a6f6fbd0b8e93a9743f7ed4ec7287fe19c415a1
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-17 07:15:09 +0000

    14_kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,20 +1,22 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
-
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+//Profile  Modelを使えるようにする
+use App\Profile;
+
+
+
+
 class ProfileController extends Controller
 {
+    
     public function add(){
         return view('admin.profile.create');
     }
     
-    public function create(){
-        return redirect('admin/profile/create');
-    }
-    
+
     public function edit(){
         return view('admin.profile.edit');
     }
@@ -22,4 +24,20 @@ class ProfileController extends Controller
     public function update(){
         return redirect('admin/profile/edit');
     }
+    
+    public function create(Request $request)
+    {
+        //Validationを行う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        //データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
+        return redirect('admin/profile/create');
+    }
+
 }
