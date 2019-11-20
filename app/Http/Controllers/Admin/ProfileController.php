3460810118ee5af9009af1d4ac03ce7739feb969
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-20 10:23:14 +0000

    16_kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,27 +17,40 @@ class ProfileController extends Controller
     }
     
 
-    public function edit(){
-        return view('admin.profile.edit');
+    public function edit(Request $request){
+       $profile = Profile::find($request->id);
+        return view('admin.profile.edit',["form"=>$profile]);
     }
     
     public function update(){
         return redirect('admin/profile/edit');
     }
     
+
+    
     public function create(Request $request)
-    {
-        //Validationを行う
-        $this->validate($request, Profile::$rules);
-        
-        $profile = new Profile;
-        $form = $request->all();
-        
-        //データベースに保存する
-        $profile->fill($form);
-        $profile->save();
-        
-        return redirect('admin/profile/create');
-    }
+   {
+      //以下を追記してVaridationを行う
+      $this->validate($request, Profile::$rules);
+      
+      $profile = new Profile;
+      $form = $request->all();
+
+      //データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      // admin/profile/createにリダイレクトする
+      return redirect('admin/profile/create');
+  }
+  
+  
+  public function delete(Request $request)
+  {
+      $profile = Profile::find($request->id);
+      $profile->delete();
+      return redirect('admin/profile/');
+  }
+  
 
 }
