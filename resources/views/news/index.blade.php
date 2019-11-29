b88b9307dadd2793a47c2c331a221b0a0ca4aab7
EC2 Default User <ec2-user@ip-172-31-30-41.us-east-2.compute.internal>
2019-11-29 07:54:17 +0000

    kadai

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <div class="container">
         <hr color="#c0c0c0">
+        <p>heroku課題</p>
         @if (!is_null($headline))
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
